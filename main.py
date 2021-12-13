c0d00b1f037e236f35b42d6525ab8c18a1621aa2
LeylaY1996 <leylay1996@gmail.com>
2021-12-14 01:07:27 +0300

    csv read

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-dkjsdmkadk
+import pandas as pd
+import numpy as np
+import nltk
+import re
+import os
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
+
+clean_data = pd.read_csv('csv/Tweets.csv')
+clean_data.head()
+
